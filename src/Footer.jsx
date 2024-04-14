0a99c2d0ce76acfd7f285b6fcf03b0b824a542a7
Ram Fiorentino <mariabf.bcn@gmail.com>
2024-04-14 14:36:05 +0200

    fixed footer video autoplay

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,11 @@
 import styles from './footer.module.css';
-import footervideo from './assets/footervideo.gif'
+import footervideo from './assets/footervideo.mp4'
 
 function Footer() {
 
     return (
       <div id={styles.footerdiv}>
-      <img width="100%" height="auto">
-        <source src={footervideo}  />
-      </img>  
+       <video autoPlay loop muted playsInline width="100%" height="auto" src={footervideo} type="video/mp4" />
       <div className={styles.containeranimationportfolio}>
               <div className={styles.horizontalscrollingitems}>
                 <div className={styles.horizontalscrollingitems__item}>
